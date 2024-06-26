29f3754f7da3e7a20ca414b8b0684b75aa952c10
Timofey Latypov <90103780+N0tilT@users.noreply.github.com>
2024-06-26 13:43:38 +0300

    Update vite.config.ts

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -4,8 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    host: "0.0.0.0",
+  server:{ 
+    host: true,
+    port: 3000,
+    proxy: {
+      '/api/':{
+        target:'http://localhost:8080',
+      },
+    },
+    watch: {
+    usePolling: true
+}
   },
   build: {
     assetsInlineLimit: 0,
